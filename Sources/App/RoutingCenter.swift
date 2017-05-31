73dfeea1c750d611d4cb59af95a2a88ca68717ff
Loic LE PENN <loic.lepenn@orange.com>
2017-05-31 15:29:41 +0200

    initialize Routing center

---
+++ Sources/App/RoutingCenter.swift
@@ -0,0 +1,14 @@
+//
+//  RoutingCenter.swift
+//  vaporMbao
+//
+//  Created by Loic LE PENN on 22/05/2017.
+//
+//
+
+import Vapor
+
+final class RoutingCenter: RouteCollection, EmptyInitializable {
+    func build(_ builder: RouteBuilder) throws {
+    }
+}
